f447cf303f8b895d645f34369d34ee69a01b2ee6
V6539 <V6539@lab.aca>
2019-09-30 09:40:53 -0500

    Base de datos de profes con distrito

--- routes/login.js
+++ routes/login.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('login/index', { 
+  res.render('login', { 
     title: 'Ingresar', 
     message_color: 'text-danger',
     message: 'Usuario y/o contraseña no válidos',
