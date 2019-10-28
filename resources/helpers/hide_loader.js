0302096a3bd7d5743d0526daf9ee373056d4a156
V6539 <V6539@lab.aca>
2019-10-28 09:36:12 -0500

    Imagend e carga al frente

--- resources/helpers/hide_loader.js
+++ resources/helpers/hide_loader.js
@@ -1,12 +1,10 @@
 const hideLoader = function(){
-  /*
   $('.preloader').addClass('complete');
   setTimeout(function(){
     $('.preloader').css('background', 'transparent'); 
     $('.wrapper').css('display', 'none'); 
     $('.preloader').css('z-index', 0); 
   }, 1000);
-  */
   if(FIRST_LOAD){
     FIRST_LOAD = false;
   }
